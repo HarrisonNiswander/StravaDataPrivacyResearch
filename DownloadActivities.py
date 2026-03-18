ac224bd3d0594d115481b23a79e80e108dc8b18f
Harrison Niswander <niswhd01@pfw.edu>
2026-03-18 17:47:30 -0400

    Athlete I data

--- DownloadActivities.py
+++ DownloadActivities.py
@@ -61,7 +61,7 @@ print(len(all_activities))
 import csv
 columnHeaders = all_activities[0].keys()
 
-with open('AthleteI.csv', mode='w', newline='', encoding='utf-8') as file:
+with open('AthleteJ.csv', mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(columnHeaders)
 
